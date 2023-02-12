ab561935f06a61f57f5dcd0d50b2019e31ef0a22
MissSage <184078718@qq.com>
2023-02-13 01:08:15 +0800

    增加瀑布流

--- src/components/MasonryLayouts.vue
+++ src/components/MasonryLayouts.vue
@@ -1,86 +1,143 @@
 <template>
-  <div class="img-gallary">
-    <div class="img-item" v-for="(item, index) in dataList" :key="index"
-      :style="{ width: '200px', left: state.imgLefts[index] + 'px', top: state.imgTops[index] + 'px' }">
-      <el-image class="image" :src="item.img" :alt="item.title" @load="init(index)">
-        <template #error>
-          <div class="image-slot">
-            <el-icon>
-              <Icon icon="mdi:picture-in-picture-bottom-right"></Icon>
-            </el-icon>
-          </div>
-        </template>
-      </el-image>
-      <div class="title">{{ item.title }}</div>
+  <div ref="refContainer" class="v-waterfall-content" id="v-waterfall">
+    <div
+      v-for="(img, i) in state.waterfallList"
+      :key="i"
+      class="v-waterfall-item"
+      :style="{
+        top: img.top + 'px',
+        left: img.left + 'px',
+        width: state.waterfallImgWidth + 'px',
+        height: img.height,
+      }"
+    >
+      <img fit="contain" :src="img.src" alt="" style="width: 100%" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { reactive, ref } from "vue"
-import { Icon } from '@iconify/vue'
-defineProps<{
-  dataList: any[]
-}>()
+import { getImageUrl } from "@/utils/UrlHelper";
+import { onBeforeMount, onMounted, reactive, ref } from "vue";
 const state = reactive<{
-  imgLefts: number[]
-  imgHeights: number[]
-  imgTops: number[]
-  top: number
+  waterfallList: any[]; //存放计算好的数据
+  waterfallImgWidth: number; //每一列的宽度
+  waterfallImgCol: number; //多少列
+  waterfallImgRight: number; //右边距
+  waterfallImgBottom: number; //下边距
+  waterfallDeviationHeight: number[]; //存放瀑布流各个列的高度
+  imgList: {
+    title: string;
+    img: string;
+  }[];
 }>({
-  imgLefts: [],
-  imgHeights: [],
-  imgTops: [],
-  top: 20
-})
-const init = async (index: number) => {
-  debugger
-  const winwidth = document.querySelector('.img-gallary')?.clientWidth||0
-  const num = Math.floor(winwidth / 220)
-  // console.log(num);
-  // for (let index = 0; index < emit.yui.length; index++) {
-  if (index < num) {
-      state.imgLefts[index] = index * (200 + 20)
-      let bili = (document.querySelector(`.img-item :nth-child(${index + 1})`) as HTMLImageElement)?.naturalWidth / 200
-      state.imgHeights[index] = (document.querySelector(`.img-item :nth-child(${index + 1})`) as HTMLImageElement).naturalHeight / bili + state.top
-      state.imgTops[index] = 0
-  } else {
-    // 记录最小高度
-    let minheight: any = state.imgHeights[0]
-    // 记录最小索引
-    let minindex: number = 0
-    // console.log(imgheight.value);
-    for (let i = 0; i < num; i++) {
-      if (state.imgHeights[i] < minheight) {
-        minheight = state.imgHeights[i]
-        minindex = i
-      }
+  waterfallList: [], //存放计算好的数据
+  waterfallImgWidth: 200, //每一列的宽度
+  waterfallImgCol: 1, //多少列
+  waterfallImgRight: 10, //右边距
+  waterfallImgBottom: 10, //下边距
+  waterfallDeviationHeight: [], //存放瀑布流各个列的高度
+  imgList: [],
+});
+const pagination = reactive<IPagination>({
+  refreshData: ({ page, size }) => {
+    pagination.page = page;
+    pagination.limit = size;
+    initData();
+  },
+});
+const initData = async () => {
+  // const res = await getMovies({
+  //   page: pagination.page || 1,
+  //   size: pagination.limit || 20
+  // })
+  // state.imgList = res.data.data
+  state.imgList = Array.from({ length: pagination.limit || 50 }).map(
+    (item, i) => {
+      const index = i % 5;
+      const imgUrl = getImageUrl(index + ".jpg");
+      return {
+        title: i.toString(),
+        img: imgUrl,
+      };
     }
-    state.imgLefts[index] = minindex * (200 + 20)
-    let bili = (document.querySelector(`.img-item :nth-child(${index + 1})`) as HTMLImageElement)?.naturalWidth / 200
-    state.imgTops[index] = state.imgHeights[minindex]
-    state.imgHeights[minindex] = (document.querySelector(`.img-item :nth-child(${index + 1})`) as HTMLImageElement)?.naturalHeight / bili + minheight + state.top
+  );
+};
+const refContainer = ref<HTMLDialogElement>();
+//计算每个图片的宽度或者是列数
+const calculationWidth = () => {
+  const domWidth = refContainer.value?.clientWidth;
+  if (domWidth && domWidth > 400) {
+    state.waterfallImgCol = Math.floor(
+      domWidth / (state.waterfallImgWidth + state.waterfallImgRight)
+    );
   }
-  // }
-}
+
+  //初始化偏移高度数组
+  state.waterfallDeviationHeight = new Array(state.waterfallImgCol);
+  state.waterfallDeviationHeight = Array.from({
+    length: state.waterfallImgCol,
+  }).map(() => 0);
+  imgPreloading();
+};
+const imgPreloading = () => {
+  debugger;
+  for (let i = 0; i < state.imgList.length; i++) {
+    let aImg = new Image();
+    aImg.src = state.imgList[i]?.img;
+    aImg.onload = aImg.onerror = (e) => {
+      let imgData: any = {};
+      //根据设定的列宽度求出图片的高度
+      imgData.height =
+        aImg.width === 0
+          ? 0
+          : (state.waterfallImgWidth / aImg.width) * aImg.height;
+      imgData.src = state.imgList[i]?.img;
+      state.waterfallList.push(imgData);
+      //调用图片位置计算方法
+      rankImg(imgData);
+    };
+  }
+};
+const rankImg = (imgData: any) => {
+  let {
+    waterfallImgWidth,
+    waterfallImgRight,
+    waterfallImgBottom,
+    waterfallDeviationHeight,
+    waterfallImgCol,
+  } = state;
+  //找出当前最短列的索引
+  const minIndex = state.waterfallDeviationHeight.indexOf(
+    Math.min(...state.waterfallDeviationHeight)
+  );
+  //获取最短列底部高度，既下一张图片的顶部高度
+  imgData.top = waterfallDeviationHeight[minIndex];
+  //计算左侧偏移，最短列索引*（右边距+列宽度）
+  imgData.left = minIndex * (waterfallImgRight + waterfallImgWidth);
+  //改变当前列高度
+  waterfallDeviationHeight[minIndex] += imgData.height + waterfallImgBottom;
+};
+onBeforeMount(() => {
+  initData();
+});
+onMounted(() => {
+  calculationWidth();
+});
 </script>
 <style lang="scss" scoped>
-.img-gallary{
-  position: relative;
-}
-.img-item {
-  position: absolute;
-}
-
-.el-image {
-  width: 100%;
-  height: calc(100% - 30px);
-}
-
-.image-slot {
+.v-waterfall-content {
   width: 100%;
   height: 100%;
-  display: grid;
-  place-items: center;
+  max-width: 100%;
+  position: relative;
+}
+.v-waterfall-item {
+  float: left;
+  position: absolute;
+}
+.v-waterfall-item img {
+  width: auto;
+  height: auto;
 }
 </style>
--- src/views/pages/movies/index.vue
+++ src/views/pages/movies/index.vue
@@ -1,48 +1,21 @@
 <template>
   <div class="layout">
     <div class="list-main">
-      <MasonryLayouts :data-list="state.dataList"></MasonryLayouts>
+      <MasonryLayouts></MasonryLayouts>
     </div>
     <Pagination :config="pagination"></Pagination>
   </div>
 </template>
 <script lang="ts" setup>
-import { onBeforeMount, reactive } from "vue";
-import { getMovies } from "../../../api";
+import { reactive } from "vue";
 import MasonryLayouts from "../../../components/MasonryLayouts.vue";
 import Pagination from "../../../components/Pagination.vue";
-import { getImageUrl } from "@/utils/UrlHelper";
-const state = reactive<{
-  dataList: any[];
-}>({
-  dataList: [],
-});
 const pagination = reactive<IPagination>({
   refreshData: ({ page, size }) => {
     pagination.page = page;
     pagination.limit = size;
-    initData();
+    // initData();
   },
-});
-const initData = async () => {
-  // const res = await getMovies({
-  //   page: pagination.page || 1,
-  //   size: pagination.limit || 20
-  // })
-  // state.dataList = res.data.data
-  state.dataList = Array.from({ length: pagination.limit || 20 }).map(
-    (item, i) => {
-      const index = i % 5;
-      const imgUrl = getImageUrl(index + ".jpg");
-      return {
-        title: i,
-        img: imgUrl,
-      };
-    }
-  );
-};
-onBeforeMount(() => {
-  initData();
 });
 </script>
 <style lang="scss" scoped>
